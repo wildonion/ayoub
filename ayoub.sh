b59997bd4f81025e786ad9c97f68ca77c21c98d2
2017-A-15 <marefimoghaddam@unixerr.com>
2022-04-30 03:23:09 -0700

    Update ayoub.sh

--- ayoub.sh
+++ ayoub.sh
@@ -10,7 +10,8 @@
 # ./ayoub.sh build --service <SERVICE_NAME> -> build and compile the selected service (game, auth, event or nft)
 # ./ayoub.sh make --service <SERVICE_NAME> -> make a service with controller, schemas, routers and service files
 # ./ayoub.sh run --service <SERVICE_NAME> --port-number <PORT> -> run a built service on desired port (game, auth, event or nft)
-# ./ayoub.sh deploy --service <SERVICE_NAME> --port-number <PORT> -> deploy the selected service on cloud using docker (needs docker compose or dockerfile and behalf of the fandogh PaaS)
+# ./ayoub.sh deploy --service <SERVICE_NAME> --port-number <PORT> -> deploy the selected compiled wasm of runtime actor on cloud using docker
+# ./ayoub.sh call  --service <SERVICE_NAME> --method-name <METHOD> -> call a serverless method from the compiled wasm of runtime actor of a selected service  
 # ./ayoub.sh list --api --controller <CONTROLLER_NAME> -> list all available apis related to a controller (game, auth, event and nft)
 
 
