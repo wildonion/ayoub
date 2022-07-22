96019d1cddc16d2205fda859b71aa8c139fce893
wildonion <marefimoghaddam@unixerr.com>
2022-07-22 18:13:46 +0430

    fix the bug of build script

--- PaaS/src/runtime/README.md
+++ PaaS/src/runtime/README.md
@@ -8,3 +8,5 @@
 ## Build: 
 
 ```sudo chmod +x build.sh && ./build.sh```
+
+> We can't compile the modules inside the `PaaS/src/Cargo.toml` to wasm, so don't put the `[lib]` section in there.
--- PaaS/src/runtime/build.sh
+++ PaaS/src/runtime/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-cargo install wasm-pack sfz
+cargo install wasm-pack --force
 sudo npm i wasm-opt -g
-wasm-pack build --target web
-# wasm-pack build --target nodejs
+wasm-pack build --target bundler --out-dir bundlerPKG
+wasm-pack build --target web --out-dir webPKG
+wasm-pack build --target nodejs --out-dir nodePKG
 wasm-opt -Oz pkg/rafael_bg.wasm -o pkg/rafael_bg.wasm # execute default optimization, passes, super-focusing on code
-wasm-opt -Oz ../target/wasm32-unknown-unknown/release/rafael.wasm -o PaaS/src/runtime/pkg/rafael_bg.wasm
-# node index.js
-# sfz -r 
+wasm-opt -Oz ../target/wasm32-unknown-unknown/release/rafael.wasm -o PaaS/src/runtime/rafael.wasm
