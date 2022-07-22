f28799dcc288f6140bbc945c00bc0f781f562f32
2017-A-15 <marefimoghaddam@unixerr.com>
2022-07-22 18:43:11 +0430

    Update build.sh

--- PaaS/src/runtime/build.sh
+++ PaaS/src/runtime/build.sh
@@ -6,4 +6,4 @@ wasm-pack build --target bundler --out-dir bundlerPKG
 wasm-pack build --target web --out-dir webPKG
 wasm-pack build --target nodejs --out-dir nodePKG
 wasm-opt -Oz pkg/rafael_bg.wasm -o pkg/rafael_bg.wasm # execute default optimization, passes, super-focusing on code
-wasm-opt -Oz ../../../target/wasm32-unknown-unknown/release/rafael.wasm -o PaaS/src/runtime/rafael.wasm
+wasm-opt -Oz ../../../target/wasm32-unknown-unknown/release/rafael.wasm -o rafael.wasm
