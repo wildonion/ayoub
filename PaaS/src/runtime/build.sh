89197f2ab0252a27d39aef33472b22ddc7100203
2017-A-15 <marefimoghaddam@unixerr.com>
2022-07-22 18:54:54 +0430

    fix the bug

--- PaaS/src/runtime/build.sh
+++ PaaS/src/runtime/build.sh
@@ -5,5 +5,7 @@ sudo npm i wasm-opt -g
 wasm-pack build --target bundler --out-dir bundlerPKG
 wasm-pack build --target web --out-dir webPKG
 wasm-pack build --target nodejs --out-dir nodePKG
-wasm-opt -Oz pkg/rafael_bg.wasm -o pkg/rafael_bg.wasm # execute default optimization, passes, super-focusing on code
+wasm-opt -Oz bundlerPKG/rafael_bg.wasm -o bundlerPKG/rafael_bg.wasm # execute default optimization, passes, super-focusing on code
+wasm-opt -Oz webPKG/rafael_bg.wasm -o webPKG/rafael_bg.wasm # execute default optimization, passes, super-focusing on code
+wasm-opt -Oz nodePKG/rafael_bg.wasm -o nodePKG/rafael_bg.wasm # execute default optimization, passes, super-focusing on code
 wasm-opt -Oz ../../../target/wasm32-unknown-unknown/release/rafael.wasm -o rafael.wasm
