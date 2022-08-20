597b45c4e8f5f2a23c616e4ef1f4873911aea0df
2017-A-15 <marefimoghaddam@unixerr.com>
2022-08-20 14:51:18 +0430

    add more api for event

--- PaaS/src/routers/event.rs
+++ PaaS/src/routers/event.rs
@@ -27,7 +27,14 @@ use hyper::{Method, Body, Response};
 use std::sync::Arc;
 use crate::controllers::event::{
                                 add::main as add_event, 
-                                get::{all as get_all_events, player_all as get_all_player_events, single as get_single_event, group_all as get_all_group_events}, 
+                                get::{all as all_events, 
+                                      all_none_expired as get_all_none_expired_events,
+                                      all_expired as get_all_expired_events,
+                                      player_all_expired as get_all_player_expired_events, 
+                                      player_all_none_expired as get_all_player_none_expired_events, 
+                                      single as get_single_event, 
+                                      group_all as get_all_group_events
+                                    }, 
                                 vote::main as cast_vote_event, 
                                 expire::main as expire_event, 
                                 _404::main as not_found, 
@@ -48,9 +55,12 @@ pub async fn register() -> Router<Body, hyper::Error>{
     Router::builder()
         .middleware(enable_cors_all())
         .post("/event/add", add_event)
-        .get("/event/get/availables", get_all_events)
+        .get("/event/get/all/in-going", get_all_none_expired_events)
+        .get("/event/get/all/done", get_all_expired_events)
+        .post("/event/get/all/player/in-going",get_all_player_none_expired_events)
+        .post("/event/get/all/player/done",get_all_player_expired_events)
         .get("/event/get/all/group", get_all_group_events)
-        .post("/event/get/all/player",get_all_player_events)
+        .get("/event/get/all", all_events)
         .post("/event/get/single", get_single_event)
         .post("/event/cast-vote", cast_vote_event)
         .post("/event/set-expire", expire_event)
