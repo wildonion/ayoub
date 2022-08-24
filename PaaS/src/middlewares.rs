65414876250fcebc47b9af382e4b6c965f113860
wildonion <marefimoghaddam@unixerr.com>
2022-08-24 17:17:34 +0430

    update to the latest changes trying to fix fucking CORSSSS

--- PaaS/src/middlewares.rs
+++ PaaS/src/middlewares.rs
@@ -31,6 +31,7 @@ pub mod cors{
                 .header(header::ACCESS_CONTROL_ALLOW_HEADERS, "*")
                 .header(header::ACCESS_CONTROL_ALLOW_METHODS, "*")
                 .header(header::ACCESS_CONTROL_EXPOSE_HEADERS, "*")
+                .header(header::ACCESS_CONTROL_ALLOW_CREDENTIALS, "*")
                 .body(Body::default())
                 .unwrap()
         ) //-- sending back an empty response to the browser to say that the preflight request was ok to get rid of the fucking CORS :)
