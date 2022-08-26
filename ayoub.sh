3883657ce1c9c1b3b7355ced7d5afb196735870f
wildonion <marefimoghaddam@unixerr.com>
2022-08-26 16:22:04 +0430

    remove docs

--- ayoub.sh
+++ ayoub.sh
@@ -20,8 +20,8 @@
 
 sudo chown -R root:root . && sudo chmod -R 777 .
 cargo build --bin ayoub --release
-sudo rm /home/ayoub/ayoub
-sudo cp target/release/ayoub /home/ayoub/ayoub
+sudo rm /home/ayoub/app
+sudo cp target/release/ayoub /home/ayoub/app
 sudo pm2 delete ayoub
 sudo pm2 start ayoub --name ayoub
 
