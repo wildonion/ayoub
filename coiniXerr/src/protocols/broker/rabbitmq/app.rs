4c742f9688beec6281331262b0353aa4b1d033a1
wildonion <marefimoghaddam@unixerr.com>
2022-10-26 15:32:59 +0330

    :-

--- coiniXerr/src/protocols/broker/rabbitmq/app.rs
+++ coiniXerr/src/protocols/broker/rabbitmq/app.rs
@@ -2,6 +2,9 @@
 
 
 
+
+
+// we use actors which use channels like mpsc to avoid race conditions to build multithreading jobq apps like rabbitmq
 // the client app to write pub/sub codes
 // rabbitmq server must be installed on the system 
 // ...
