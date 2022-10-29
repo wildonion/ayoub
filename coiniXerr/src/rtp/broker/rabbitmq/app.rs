ffd1a28c672bbda34b703fb5b22264a484e9f1a9
wildonion <marefimoghaddam@unixerr.com>
2022-10-30 01:06:57 +0330

    scaffold hoopoe

--- coiniXerr/src/rtp/broker/rabbitmq/app.rs
+++ coiniXerr/src/rtp/broker/rabbitmq/app.rs
@@ -8,12 +8,15 @@
 // we use multithreading design patterns like actors which use channels like mpsc to avoid race conditions to build multithreading jobq apps like rabbitmq
 // the client app to write pub/sub codes like twitter tweets broadcaster and food ordering service on top of the following protocols
 
-
 use crate::*;
 use rtp::{
-        rpc::server as rpc_server,
-        wrtc::server as wrtc_server,
+    rpc::server as rpc_server,
+    wrtc::server as wrtc_server,
         ws::server as ws_server,
         socks::server as socks_server,
         p2p::udp::app as p2p_app,
     };
+        
+    
+// this is the publisher code of hoopoe
+// ...
